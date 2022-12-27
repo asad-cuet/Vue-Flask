8b7201721c8b18eec3f4ae512adbc8bb0c4fa165
Asadul Islam Hamzah <68386760+asad-cuet@users.noreply.github.com>
2022-12-27 16:25:42 +0600

    init

---
+++ src/views/csvAPI.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <div>
+    <h2>API</h2>
+    <div v-for="data in datas" :key="data.id">
+        Name: {{ data.name }} <br>
+        Author: {{ data.author }} <br>
+    </div>
+    </div>
+
+</template>
+<script>
+import {ref} from 'vue'
+import axios from 'axios'
+export default {
+    name:'csvAPI',
+    setup() {
+        const datas=ref(null)
+        const error=ref(null)
+        const load= async() => {
+            axios.get('http://127.0.0.1:5000/books')
+            .then((res) => {
+                console.log(res.data)
+                datas.value=res.data
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+        }
+
+        load()
+
+        return {
+            datas
+        }
+    }
+}
+
+</script>
